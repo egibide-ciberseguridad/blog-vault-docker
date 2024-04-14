e107a1d72561cbf79c75844c088b8de5a4230df4
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-04-15 00:26:25 +0200

    Reordenar los métodos

--- laravel-blog/app/Http/Middleware/VaultCredentials.php
+++ laravel-blog/app/Http/Middleware/VaultCredentials.php
@@ -15,6 +15,51 @@ use Illuminate\Support\Facades\Storage;
 
 class VaultCredentials
 {
+    public function handle(Request $request, Closure $next)
+    {
+        $nueva_concesion = true;
+
+        $id_concesion = Cache::get('id_concesion');
+
+        if (isset($id_concesion)) {
+            $estado_concesion = $this->leer_estado($id_concesion);
+            if (isset($estado_concesion)) {
+                $ttl = $this->tiempo_restante($estado_concesion['data']['expire_time']);
+                if ($ttl > 60) {
+                    $nueva_concesion = false;
+                } else {
+                    $this->revocar_concesion($id_concesion);
+                    Cache::forget('id_concesion');
+                }
+            }
+        }
+
+        if ($nueva_concesion) {
+            $concesion = $this->vault_read(config('vault.db_credential'));
+
+            if (!isset($concesion)) {
+                throw new Exception("Error de Vault: no se han podido obtener credenciales");
+            }
+
+            $this->guardar_configuracion($concesion['data']['username'], 'db_username');
+            $this->guardar_configuracion($concesion['data']['password'], 'db_password');
+
+            $id_concesion = $concesion['lease_id'];
+            $estado_concesion = $this->leer_estado($id_concesion);
+            Cache::put('id_concesion', $id_concesion);
+        }
+
+        $this->cargar_configuracion('database.connections.mysql.username', 'db_username');
+        $this->cargar_configuracion('database.connections.mysql.password', 'db_password');
+
+        $ttl = $this->tiempo_restante($estado_concesion['data']['expire_time']);
+        if ($ttl < 60) {
+            $this->renovar_concesion($id_concesion);
+        }
+
+        return $next($request);
+    }
+
     private $cliente = null;
     private $headers = null;
 
@@ -70,66 +115,21 @@ class VaultCredentials
         return $result;
     }
 
-    public function handle(Request $request, Closure $next)
-    {
-        $nueva_concesion = true;
-
-        $id_concesion = Cache::get('id_concesion');
-
-        if (isset($id_concesion)) {
-            $estado_concesion = $this->leer_estado($id_concesion);
-            if (isset($estado_concesion)) {
-                $ttl = $this->tiempo_restante($estado_concesion['data']['expire_time']);
-                if ($ttl > 60) {
-                    $nueva_concesion = false;
-                } else {
-                    $this->revocar_concesion($id_concesion);
-                    Cache::forget('id_concesion');
-                }
-            }
-        }
-
-        if ($nueva_concesion) {
-            $concesion = $this->vault_read(config('vault.db_credential'));
-
-            if (!isset($concesion)) {
-                throw new Exception("Error de Vault: no se han podido obtener credenciales");
-            }
-
-            $this->guardar_configuracion($concesion['data']['username'], 'db_username');
-            $this->guardar_configuracion($concesion['data']['password'], 'db_password');
-
-            $id_concesion = $concesion['lease_id'];
-            $estado_concesion = $this->leer_estado($id_concesion);
-            Cache::put('id_concesion', $id_concesion);
-        }
-
-        $this->cargar_configuracion('database.connections.mysql.username', 'db_username');
-        $this->cargar_configuracion('database.connections.mysql.password', 'db_password');
-
-        $ttl = $this->tiempo_restante($estado_concesion['data']['expire_time']);
-        if ($ttl < 60) {
-            $this->renovar_concesion($id_concesion);
-        }
-
-        return $next($request);
-    }
-
-    public function renovar_concesion(mixed $lease_id): void
+    private function renovar_concesion(mixed $lease_id): void
     {
         $this->vault_write('sys/leases/renew', [
             "lease_id" => $lease_id,
         ]);
     }
 
-    public function revocar_concesion(mixed $lease_id): void
+    private function revocar_concesion(mixed $lease_id): void
     {
         $this->vault_write('sys/leases/revoke', [
             "lease_id" => $lease_id,
         ]);
     }
 
-    public function leer_estado(mixed $lease_id): mixed
+    private function leer_estado(mixed $lease_id): mixed
     {
         $estado_concesion = $this->vault_write('sys/leases/lookup', [
             "lease_id" => $lease_id,
@@ -137,7 +137,7 @@ class VaultCredentials
         return $estado_concesion;
     }
 
-    public function tiempo_restante($expire_time): int|float
+    private function tiempo_restante($expire_time): int|float
     {
         $fecha_caducidad = Carbon::parse($expire_time);
 
@@ -145,13 +145,13 @@ class VaultCredentials
         return $ttl;
     }
 
-    public function cargar_configuracion($config_key, $temp_file): void
+    private function cargar_configuracion($config_key, $temp_file): void
     {
         $username = Storage::disk('temp')->get($temp_file);
         Config::set($config_key, $username);
     }
 
-    public function guardar_configuracion($config_value, $temp_file): void
+    private function guardar_configuracion($config_value, $temp_file): void
     {
         Storage::disk('temp')->put($temp_file, $config_value);
     }
