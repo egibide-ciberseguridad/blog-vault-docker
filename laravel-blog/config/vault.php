7c4330bacb8fe5f8f8a76289b73fbad8a330c926
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-12-21 10:42:08 +0100

    Leer el token de Vault desde un secreto

--- laravel-blog/config/vault.php
+++ laravel-blog/config/vault.php
@@ -1,7 +1,19 @@
 <?php
 
+function read_docker_secret(string $name): string
+{
+    return trim(@file_get_contents('/run/secrets/' . $name));
+}
+
+function docker_secret(string $name): Closure
+{
+    return function () use ($name) {
+        return read_docker_secret($name);
+    };
+}
+
 return [
     'url' => env('VAULT_ADDR', 'http://vault-server:8200'),
-    'token' => env('VAULT_TOKEN', 'root'),
+    'token' => env('VAULT_TOKEN', docker_secret('vault_token')),
     'db_credential' => env('VAULT_DB_CREDENTIAL', 'database/creds/blog'),
 ];
